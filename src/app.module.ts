4e6e15bf7905ee4986b531bacfa27c5f6f5804d0
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-11-02 19:29:48 +0200

    connected to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,21 @@ import { AuthModule } from './modules/auth/auth.module';
 import { BrandModule } from './modules/brand/brand.module';
 import { ProductModule } from './modules/product/product.module';
 import { CategoryModule } from './modules/category/category.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import devConfig from './config/env/dev.config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [devConfig],
       isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('db').url,
+      }),
     }),
     AuthModule,
     BrandModule,

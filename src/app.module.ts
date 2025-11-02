02b846de8b4bbcc618ff7ed00617a7d3302ff1bc
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-11-02 16:41:59 +0200

    add aut , category , brand and proudct resourcies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { BrandModule } from './modules/brand/brand.module';
+import { ProductModule } from './modules/product/product.module';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
-    imports: [],
+    imports: [AuthModule, BrandModule, ProductModule, CategoryModule],
     controllers: [AppController],
     providers: [AppService],
 })

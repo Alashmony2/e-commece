a212d52617feb743cf3fbd01aec897e76e0d85d9
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-11-03 18:12:17 +0200

    register customer model in mongoose

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import {
+  Customer,
+  CustomerRepository,
+  customerSchema,
+  User,
+  userSchema,
+} from 'src/models';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: userSchema,
+        discriminators: [{ name: Customer.name, schema: customerSchema }],
+      },
+    ]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, CustomerRepository],
 })
 export class AuthModule {}

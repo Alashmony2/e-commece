a019eee90ea34022fd1adef64389143667b1771c
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-11-15 15:50:50 +0200

    create createCategory service

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { Category, CategoryRepository, categorySchema } from '@models/index';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoryFactoryService } from './factory';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports:[MongooseModule.forFeature([{name:Category.name,schema:categorySchema}])],
   controllers: [CategoryController],
-  providers: [CategoryService],
+  providers: [CategoryService,CategoryRepository,CategoryFactoryService,JwtService],
 })
 export class CategoryModule {}
